fcd6df2a3f1ba54dd912bd2fecf5a0af100486ce
Ahmed Ismail <Ahmid.ismail@gmail.com>
2021-02-20 21:16:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import React ,{Component} from 'react';
+import {Navbar, NavbarBrand} from 'reactstrap';
+
+import Menu from './components/MenuComponent'
+
+import './App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+            <Navbar dark color="primary">
+              <NavbarBrand href="/">
+                Ristorante confusion
+              </NavbarBrand>
+            </Navbar>
+            <Menu/>
+        </div>
+    )
+  }
+}
+
+export default App;
